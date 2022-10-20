5f5b20a3da06c5db739911fbf799f0d03a99f2e6
Stefan de Groot <stefan@wubdev.nl>
2022-10-20 15:16:16 +0200

    add i18n + pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  buildModules: [
+    '@intlify/nuxt3'
+  ],
+  intlify: {
+    localeDir: 'locales',
+    vueI18n: {
+      locale: 'nl',
+      fallbackLocale: 'nl',
+      availableLocales: ['nl', 'en']
+    }
+  },
 })
