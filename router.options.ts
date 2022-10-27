2261563dfebf707ef5d8d0b2bc3a2682eeb0dc70
Stefan de Groot <stefan@wubdev.nl>
2022-10-27 08:55:22 +0200

    add router options

---
+++ router.options.ts
@@ -0,0 +1,8 @@
+import type { RouterConfig } from '@nuxt/schema'
+
+export default <RouterConfig>{
+  stringifyQuery: (query) => {
+    const keys = Object.keys(query)
+    return keys.map(key => `${key}=${encodeURIComponent(query[key] as string)}`).join('&')
+  }
+}
