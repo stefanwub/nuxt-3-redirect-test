000d2a4b30c6fc87f9f899e2c4d3f300cec40ee8
Stefan de Groot <info@stefandegroot.com>
2023-01-05 15:42:16 +0100

    another test

--- pages/hello.vue
+++ pages/hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <a href="https://www.instagram.com/p/CiZzZB-IFEf/">Test link instagram</a>
-    <a href="instagram://www.instagram.com/p/CiZzZB-IFEf/">Test link instagram</a>
+    <a href="https://www.instagram.com/p/CiZzZB-IFEf/">Test link instagram</a><br>
+    <a href="instagram://media?id=2895372213574407113">App</a><br>
+    <a href="instagram://instagram.com/CiZzZB-IFEf/">No wwww</a>
   </div>
 </template>
