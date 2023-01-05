bd612728883accd76b10b0f881f3427da6a68f66
Stefan de Groot <info@stefandegroot.com>
2023-01-05 15:22:15 +0100

    add test for insta links

--- pages/hello.vue
+++ pages/hello.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    Test
+    <a href="https://www.instagram.com/p/CiZzZB-IFEf/">Test link instagram</a>
+    <a href="instagram://www.instagram.com/p/CiZzZB-IFEf/">Test link instagram</a>
   </div>
 </template>
