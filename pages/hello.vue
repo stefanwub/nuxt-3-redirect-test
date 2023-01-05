40cf01ed3858b84bbb75e90f622818657c567e1b
Stefan de Groot <info@stefandegroot.com>
2023-01-05 15:52:05 +0100

    ets

--- pages/hello.vue
+++ pages/hello.vue
@@ -3,6 +3,7 @@
     <a href="https://www.instagram.com/p/CiZzZB-IFEf/">Test link instagram</a><br>
     <a href="instagram://media?id=2895372213574407113">App</a><br>
     <a href="instagram://media?id=17898918587641288">App 2</a><br>
+    <a href="instagram://media?id=2925595453428879647">App 3</a><br>
     <a href="instagram://instagram.com/CiZzZB-IFEf/">No wwww</a>
   </div>
 </template>
